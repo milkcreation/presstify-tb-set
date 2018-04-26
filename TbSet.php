ce2e3be4a17e8b947f0968f8d0c66f14554bb0a1
Jordy Manner <jordy.manner@milkcreation.fr>
2018-04-26 13:36:18 +0000

    tb-set -- trunk -- deploiement initial 1.1

--- TbSet.php
+++ TbSet.php
@@ -6,13 +6,13 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstify-plugins/tb-set
  * @namespace \tiFy\Plugins\TbSet
- * @version 1.0.0
+ * @version 1.1.0
  */
 
 namespace tiFy\Plugins\TbSet;
 
 use tiFy\App\Plugin;
-use tiFy\Components;
+use tiFy\Plugins;
 use tiFy\Core\ScriptLoader\ScriptLoader as tFyScriptLoader;
 
 class TbSet extends Plugin
@@ -26,91 +26,9 @@ class TbSet extends Plugin
     {
         parent::__construct();
 
-        $this->appAddAction('tify_components_register');
         $this->appAddAction('init');
         $this->appAddAction('theme_before_enqueue_scripts');
         $this->appAddAction('admin_enqueue_scripts');
-    }
-
-    /**
-     * Déclaration de composants.
-     *
-     * @return void
-     */
-    public function tify_components_register()
-    {
-        Components::register(
-            'AdminUI',
-            [
-                'admin_bar_menu_logo' => [
-                    [
-                        'id'    => 'tb-logo',
-                        'title' => '<span class="tigreblanc-logo" style="font-size:35px;"></span>',
-                        'href'  => 'http://www.tigreblanc.fr',
-                        'meta'  => [
-                            'title'  => __('A propos de Tigre Blanc', 'tify'),
-                            'target' => '_blank',
-                        ],
-                    ],
-                    [
-                        'id'     => 'tb-logo-site',
-                        'parent' => 'tb-logo',
-                        'title'  => __('Site Officiel de Tigre Blanc', 'tify'),
-                        'href'   => 'http://www.tigreblanc.fr',
-                    ],
-                    [
-                        'id'     => 'tb-logo-external',
-                        'parent' => 'tb-logo',
-                        'group'  => true,
-                        'meta'   => [
-                            'class' => 'ab-sub-secondary',
-                        ],
-                    ],
-                    [
-                        'id'     => 'tb-logo-facebook',
-                        'parent' => 'tb-logo-external',
-                        'title'  => __('Page Facebook', 'tify'),
-                        'href'   => 'http://www.facebook.com/tigreblancdouai',
-                        'meta'   => [
-                            'target' => '_blank',
-                        ],
-                    ],
-                    [
-                        'id'     => 'tb-logo-twitter',
-                        'parent' => 'tb-logo-external',
-                        'title'  => __('Compte Twitter', 'tify'),
-                        'href'   => 'https://twitter.com/TigreBlancDouai',
-                        'meta'   => [
-                            'target' => '_blank',
-                        ],
-                    ],
-                    [
-                        'id'     => 'tb-logo-mailing',
-                        'parent' => 'tb-logo',
-                        'group'  => true,
-                        'meta'   => [
-                            'class' => 'ab-sub-primary',
-                        ],
-                    ],
-                    [
-                        'id'     => 'tb-logo-mailing-contact',
-                        'parent' => 'tb-logo-mailing',
-                        'title'  => __('Contact l\'agence', 'tify'),
-                        'href'   => 'mailto:contact@tigreblanc.fr',
-                    ],
-                    [
-                        'id'     => 'tb-logo-mailing-support',
-                        'parent' => 'tb-logo-mailing',
-                        'title'  => __('Support Technique', 'tify'),
-                        'href'   => 'mailto:support@tigreblanc.fr',
-                    ],
-                ],
-                'admin_footer_text'   => sprintf(
-                    __('Merci de faire de %s le partenaire de votre communication digitale', 'tify'),
-                    "<a class=\"tigreblanc-logo\" href=\"http://www.tigreblanc.fr\" style=\"font-size:40px; vertical-align:middle; display:inline-block;\" target=\"_blank\"></a>"
-                )
-            ]
-        );
     }
 
     /**
@@ -121,9 +39,9 @@ class TbSet extends Plugin
     public function init()
     {
         tFyScriptLoader::register_style(
-            'FontTb',
+            'TbSetPluginFont',
             [
-                'src' => self::tFyAppUrl() . '/assets/fonts/tb/styles.css',
+                'src' => $this->appUrl() . '/assets/fonts/tb/styles.css',
             ]
         );
     }
@@ -135,7 +53,9 @@ class TbSet extends Plugin
      */
     public function theme_before_enqueue_scripts()
     {
-        \wp_enqueue_style('FontTb');
+        if ($this->appConfig('wp_enqueue_style', true)) :
+            \wp_enqueue_style('TbSetPluginFont');
+        endif;
     }
 
     /**
@@ -145,6 +65,6 @@ class TbSet extends Plugin
      */
     final public function admin_enqueue_scripts()
     {
-        \wp_enqueue_style('FontTb');
+        \wp_enqueue_style('TbSetPluginFont');
     }
 }
