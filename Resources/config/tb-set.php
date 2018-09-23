ecdaa3318b53241639e812c6f06bd2ece0250c7b
Jordy Manner <jordy.manner@milkcreation.fr>
2018-09-23 12:57:02 +0000

    tb-set -- branch 2.0 -- réécriture

--- Resources/config/tb-set.php
+++ Resources/config/tb-set.php
@@ -7,13 +7,13 @@
 return [
     /**
      * Mise en file automatique des scripts de l'interface d'administration.
-     * {@internal Ajouter "import 'presstify-plugins/tb-set/Resources/assets/index.js';" à votre feuille de style admin}
+     * {@internal Chargement avec webpack: "import 'presstify-plugins/tb-set/Resources/assets/index.js';"}
      */
     'admin_enqueue_scripts' => true,
 
     /**
      * Mise en file automatique des scripts de l'interface utilisateur.
-     * {@internal Ajouter "import 'presstify-plugins/tb-set/Resources/assets/index.js';" à votre feuille de style global}
+     * {@internal Chargement avec webpack: "import 'presstify-plugins/tb-set/Resources/assets/index.js';"}
      */
     'wp_enqueue_scripts' => true
 ];
