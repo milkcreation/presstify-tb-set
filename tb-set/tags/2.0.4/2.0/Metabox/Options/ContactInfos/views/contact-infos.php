56f053bee619b5f979bff4229e76e3e8df100739
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-09-23 17:28:09 +0000

    tb-set -- tags 2.0.4 -- correctif actif sans config

---
+++ tb-set/tags/2.0.4/2.0/Metabox/Options/ContactInfos/views/contact-infos.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @var tiFy\Contracts\Views\ViewInterface $this.
+ */
+?>
+
+<table class="form-table">
+    <tbody>
+
+    <?php $this->insert('company', $this->all()); ?>
+
+    <?php $this->insert('address1', $this->all()); ?>
+
+    <?php $this->insert('address2', $this->all()); ?>
+
+    <?php $this->insert('address3', $this->all()); ?>
+
+    <?php $this->insert('city', $this->all()); ?>
+
+    <?php $this->insert('postcode', $this->all()); ?>
+
+    <?php $this->insert('phone', $this->all()); ?>
+
+    <?php $this->insert('fax', $this->all()); ?>
+
+    <?php $this->insert('mail', $this->all()); ?>
+
+    </tbody>
+</table>
