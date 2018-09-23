8aa8518c58045042ee8459fe761242111ea94fb7
Jordy Manner <jordy.manner@milkcreation.fr>
2018-09-23 12:57:40 +0000

    tb-set -- branch 2.0 -- réécriture

--- TbSet.php
+++ TbSet.php
@@ -6,7 +6,7 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstify-plugins/tb-set
  * @namespace \tiFy\Plugins\TbSet
- * @version 2.0.3
+ * @version 2.0.4
  */
 
 namespace tiFy\Plugins\TbSet;
