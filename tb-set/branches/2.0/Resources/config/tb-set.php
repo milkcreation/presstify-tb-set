8678078f9ea249e5f904f2c23913cd15404741bc
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-07-09 16:40:46 +0000

    tb-set -- branch 2.0 -- correctif comm. config

--- tb-set/branches/2.0/Resources/config/tb-set.php
+++ tb-set/branches/2.0/Resources/config/tb-set.php
@@ -7,14 +7,14 @@
 return [
     /**
      * Mise en file automatique des scripts de l'interface d'administration.
-     * {@internal Chargement avec webpack: "import 'presstify-plugins/tb-set/Resources/assets/index.js';"}
+     * {@internal Chargement webpack: "import 'presstify-plugins/tb-set/Resources/assets/index';"}
      * @var boolean
      */
     'admin_enqueue_scripts' => true,
 
     /**
      * Mise en file automatique des scripts de l'interface utilisateur.
-     * {@internal Chargement avec webpack: "import 'presstify-plugins/tb-set/Resources/assets/index.js';"}
+     * {@internal Chargement webpack: "import 'presstify-plugins/tb-set/Resources/assets/index';"}
      * @var boolean
      */
     'wp_enqueue_scripts' => true,
