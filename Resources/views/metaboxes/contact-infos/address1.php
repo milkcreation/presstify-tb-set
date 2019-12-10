2baa8825742cb93d4154e764c751fb6ac6bfbe19
Jordy Manner <jordy.manner@milkcreation.fr>
2019-12-10 14:32:23 +0000

    tb-set -- branch 1.4 -- prédéploiement adaptation Bridge

---
+++ Resources/views/metaboxes/contact-infos/address1.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var tiFy\Contracts\Metabox\MetaboxView $this
+ */
+?>
+<tr>
+    <th scope="row"><?php _e('Adresse ligne #1', 'tify'); ?></th>
+    <td>
+        <?php echo field('text', [
+            'attrs' => [
+                'class' => '%s widefat',
+            ],
+            'name'  => $this->name() . '[address1]',
+            'value' => $this->value('address1', ''),
+        ]); ?>
+    </td>
+</tr>
