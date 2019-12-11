4c667b6aa9010bf7353c816a3913fe9874dd8894
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-12-11 13:47:20 +0000

    tb-set -- tag 1.4.27 -- version bridgée basée sur 2.0.27

---
+++ tb-set/tags/1.4.27/TbSetServiceProvider.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\TbSet;
+
+use tiFy\Container\ServiceProvider;
+use tiFy\Plugins\TbSet\{
+    Metaboxes\ComingSoon as ComingSoonMetabox,
+    Metaboxes\ContactInfos as ContactInfosMetabox
+};
+use tiFy\Support\Proxy\Metabox;
+
+class TbSetServiceProvider extends ServiceProvider
+{
+    /**
+     * Liste des noms de qualification des services fournis.
+     * {@internal Permet le chargement différé des services qualifié.}
+     * @var string[]
+     */
+    protected $provides = [
+        'tb-set',
+        'tb-set.coming-soon'
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function boot(): void
+    {
+        add_action('after_setup_theme', function () {
+            $this->getContainer()->get('tb-set');
+            $this->getContainer()->get('tb-set.coming-soon');
+
+            if (config('tb-set.coming-soon.admin')) {
+                Metabox::registerDriver('tb-set.coming-soon', new ComingSoonMetabox());
+            }
+
+            if (config('tb-set.contact-infos.admin')) {
+                Metabox::registerDriver('tb-set.contact-infos', new ContactInfosMetabox());
+            }
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function register(): void
+    {
+        $this->getContainer()->share('tb-set', function () {
+            return new TbSet($this->getContainer()->get('app'));
+        });
+
+        $this->getContainer()->share('tb-set.coming-soon', function () {
+            return new ComingSoon();
+        });
+    }
+}
