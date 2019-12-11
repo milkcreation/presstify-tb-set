79432b856cccc71b15037884690b50e071adbe5e
Jordy Manner <jordy.manner@milkcreation.fr>
2019-12-11 13:47:20 +0000

    tb-set -- tag 1.4.27 -- version bridgée basée sur 2.0.27

---
+++ Resources/config/tb-set.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Exemple de configuration.
+ */
+
+return [
+    /**
+     * Mise en file automatique des scripts de l'interface d'administration.
+     * {@internal Chargement webpack: "import 'presstify-plugins/tb-set/Resources/assets/index';"}
+     * @var boolean
+     */
+    'admin_enqueue_scripts' => false,
+
+    /**
+     * Mise en file automatique des scripts de l'interface utilisateur.
+     * {@internal Chargement webpack: "import 'presstify-plugins/tb-set/Resources/assets/index';"}
+     * @var boolean
+     */
+    'wp_enqueue_scripts' => false,
+
+    /**
+     * Activation du message de site en contruction.
+     * @var boolean|array
+     */
+    'coming-soon' => [
+        'offline' => 'off',
+        'admin'   => true
+    ],
+
+    /**
+     * Code Google Analytics.
+     * @var string
+     */
+    'ua_code' => ''
+];
