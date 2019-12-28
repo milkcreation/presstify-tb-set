d4dbf4d07027ae2c8db5f0d97645c020b87ce33d
Jordy Manner <jordy.manner@milkcreation.fr>
2019-12-28 12:31:08 +0000

    tb-set -- branch 2.0 -- Réécriture tiFy View

--- Resources/views/ua-code/index.php
+++ Resources/views/ua-code/index.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * @var tiFy\Contracts\View\ViewController
- * @var string $ua
+ * @var tiFy\Contracts\View\PlatesFactory $this
  */
 ?>
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $ua; ?>"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $this->get('ua'); ?>"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', '<?php echo $ua; ?>');
+    gtag('config', '<?php echo $this->get('ua'); ?>');
 </script>
