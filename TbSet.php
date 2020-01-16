dcffc88b2ed82973a3afa7c60aff07f00a85a187
Jordy Manner <jordy.manner@milkcreation.fr>
2020-01-16 06:11:05 +0000

    tb-set -- branch 2.0 -- Passage dépôt en https

--- TbSet.php
+++ TbSet.php
@@ -11,7 +11,7 @@ use WP_Admin_Bar;
  * @desc Extension PresstiFy de jeux de fonctionnalités des sites TigreBlanc.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy\Plugins\TbSet
- * @version 2.0.31
+ * @version 2.0.32
  *
  * USAGE :
  * Activation
