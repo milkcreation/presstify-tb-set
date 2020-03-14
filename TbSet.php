b27de1fe285f81c525acc8096452f57311dfa769
Jordy Manner <jordy.manner@milkcreation.fr>
2020-03-14 08:40:41 +0000

    tb-set -- 2.0 -- Evolution ContactInfos

--- TbSet.php
+++ TbSet.php
@@ -11,7 +11,7 @@ use WP_Admin_Bar;
  * @desc Extension PresstiFy de jeux de fonctionnalités des sites TigreBlanc.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy\Plugins\TbSet
- * @version 2.0.33
+ * @version 2.0.34
  *
  * USAGE :
  * Activation
