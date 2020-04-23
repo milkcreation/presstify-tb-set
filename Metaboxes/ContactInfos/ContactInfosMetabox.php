88c4683ab681216655c4da76a6f5f878715e7129
Jordy Manner <jordy.manner@milkcreation.fr>
2020-04-23 06:03:31 +0000

    tb-set -- 2.0.35 -- Passe contact-infos > Gestion group && fields

---
+++ Metaboxes/ContactInfos/ContactInfosMetabox.php
@@ -0,0 +1,259 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\TbSet\Metaboxes\ContactInfos;
+
+use Illuminate\Support\Collection;
+use tiFy\Plugins\TbSet\AbstractMetaboxDriver;
+
+class ContactInfosMetabox extends AbstractMetaboxDriver
+{
+    /**
+     * @inheritDoc
+     */
+    protected $alias = 'contact-infos';
+
+    /**
+     * Liste des instances de champs déclarés.
+     * @var ContactInfosField[]|array
+     */
+    protected $fields = [];
+
+    /**
+     * Liste des instances de groupe de champs déclarés.
+     * @var ContactInfosGroup[]|array
+     */
+    protected $groups = [];
+
+    /**
+     * Déclaration d'un champ.
+     *
+     * @param string $alias
+     * @param array|ContactInfosField $args
+     *
+     * @return $this
+     */
+    public function addField(string $alias, $args = []): self
+    {
+        $field = $args instanceof ContactInfosField ? $args : (new ContactInfosField())->set($args);
+
+        $this->fields[$alias] = $field->setMetabox($this)->setAlias($alias);
+
+        return $this;
+    }
+
+    /**
+     * Déclaration d'un groupe de champ.
+     *
+     * @param string $alias
+     * @param array|ContactInfosGroup $args
+     *
+     * @return $this
+     */
+    public function addGroup(string $alias, $args = []): self
+    {
+        $group = $args instanceof ContactInfosGroup ? $args : (new ContactInfosGroup())->set($args);
+
+        $this->groups[$alias] = $group->setMetabox($this)->setAlias($alias);
+
+        return $this;
+    }
+
+    /**
+     * Initialisation de la boîte de saisie.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        parent::boot();
+
+        $fields = [
+            'address1' => [
+                'group'    => 'contact',
+                'name'     => 'contact_address1',
+                'title'    => __('Adresse Postale', 'tify')
+            ],
+            'address2' => [
+                'group'    => 'contact',
+                'name'     => 'contact_address2',
+                'title'    => __('Adresse complémentaire', 'tify')
+            ],
+            'address3' => [
+                'group'    => 'contact',
+                'name'     => 'contact_address3',
+                'title'    => __('Informations supplémentaires concernant l\'adresse', 'tify')
+            ],
+            'city'     => [
+                'group'    => 'contact',
+                'name'     => 'contact_city',
+                'title'    => __('Ville', 'tify')
+            ],
+            'postcode' => [
+                'group'    => 'contact',
+                'name'     => 'contact_postcode',
+                'title'    => __('Code postal', 'tify')
+            ],
+            'phone'    => [
+                'group'    => 'contact',
+                'name'     => 'contact_phone',
+                'title'    => __('Numéro de téléphone', 'tify')
+            ],
+            'fax'      => [
+                'group'    => 'contact',
+                'name'     => 'contact_fax',
+                'title'    => __('Numéro de fax', 'tify')
+            ],
+            'email'    => [
+                'group'    => 'contact',
+                'name'     => 'contact_email',
+                'title'    => __('Adresse de messagerie', 'tify')
+            ],
+            'website'    => [
+                'group'    => 'contact',
+                'name'     => 'contact_website',
+                'title'    => __('Site internet', 'tify')
+            ],
+            'map'      => [
+                'group' => 'contact',
+                'name'  => 'contact_map',
+                'title' => __('Carte', 'theme'),
+            ],
+            'maplink'  => [
+                'group' => 'contact',
+                'name'  => 'contact_maplink',
+                'title' => __('Lien vers la carte interactive', 'theme'),
+            ],
+            'company'  => [
+                'group'    => 'company',
+                'name'     => 'company_name',
+                'title'    => __('Nom de la société', 'tify')
+            ],
+            'form'     => [
+                'group'    => 'company',
+                'name'     => 'company_form',
+                'title'    => __('Forme juridique', 'tify')
+            ],
+            'siren'    => [
+                'group'    => 'company',
+                'name'     => 'company_siren',
+                'title'    => __('Numéro de SIREN', 'tify')
+            ],
+            'siret'    => [
+                'group'    => 'company',
+                'name'     => 'company_siret',
+                'title'    => __('Numéro de SIRET', 'tify')
+            ],
+            'tva'      => [
+                'group'    => 'company',
+                'name'     => 'company_tva',
+                'title'    => __('N° de TVA Intracommunautaire', 'tify')
+            ],
+            'ape'      => [
+                'group'    => 'company',
+                'name'     => 'company_ape',
+                'title'    => __('Activité (Code NAF ou APE)', 'tify')
+            ],
+            'cnil'     => [
+                'group'    => 'company',
+                'name'     => 'company_cnil',
+                'title'    => __('Déclaration CNIL', 'tify')
+            ],
+            'opening'  => [
+                'group' => 'company',
+                'name'  => 'company_opening',
+                'title' => __('Horaires d\'ouverture', 'theme'),
+            ]
+        ];
+
+        foreach ($fields as $alias => $field) {
+            $this->addField($alias, $field);
+        }
+
+        $groups = [
+            'contact' => [
+                'title'    => __('Informations de contact', 'theme'),
+            ],
+            'company' => [
+                'title'    => __('Informations sur la société', 'theme'),
+            ],
+        ];
+
+        foreach ($groups as $alias => $group) {
+            $this->addGroup($alias, $group);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function defaults(): array
+    {
+        return array_merge(parent::defaults(), [
+            'name'  => 'contact_infos',
+            'title' => __('Informations de contact', 'tify'),
+        ]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function defaultParams(): array
+    {
+        return [
+            'fields' => ['address1', 'address2', 'city', 'postcode', 'phone', 'email'],
+            'groups' => ['contact'],
+        ];
+    }
+
+    /**
+     * Récupération de la liste des champs associé à un groupe.
+     *
+     * @param string $group Alias de qualification du groupe.
+     *
+     * @return ContactInfosField[]|array
+     */
+    public function getFields(string $group): array
+    {
+        $exists = $this->params('fields', []);
+        $fields = [];
+
+        foreach ($exists as $alias) {
+            if (isset($this->fields[$alias])) {
+                $fields[$alias] = $this->fields[$alias]->prepare();
+            }
+        }
+
+        return (new Collection($fields))->filter(function ($field) use ($group) {
+            return ($field->group === $group) ? true : false;
+        })->sortBy('position')->all();
+    }
+
+    /**
+     * Récupération de la liste des groupes de champs.
+     *
+     * @return ContactInfosGroup[]|array
+     */
+    public function getGroups(): array
+    {
+        $aliases = $this->params('groups', []);
+        $groups = [];
+
+        foreach ($aliases as $alias) {
+            if (isset($this->groups[$alias])) {
+                $groups[$alias] = $this->groups[$alias]->prepare();
+            }
+        }
+
+        return (new Collection($groups))->sortBy('position')->all();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function render(): string
+    {
+        $this->set('groups', $this->getGroups());
+
+        return parent::render();
+    }
+}
--- TbSet.php
+++ TbSet.php
@@ -1,162 +1,202 @@
-<?php
-
-/**
- * @name TbSet
- * @desc Extension PresstiFy de jeu de fonctionnalités associées aux sites TigreBlanc.
- * @author Jordy Manner <jordy@milkcreation.fr>
- * @package presstify-plugins/tb-set
- * @namespace \tiFy\Plugins\TbSet
- * @version 2.0.1
- */
+<?php declare(strict_types=1);
 
 namespace tiFy\Plugins\TbSet;
 
-use tiFy\Apps\AppController;
-use tiFy\Apps\AppsServiceProvider;
-use tiFy\Plugins\AdminUi\AdminUi;
+use Psr\Container\ContainerInterface as Container;
+use tiFy\Plugins\TbSet\Contracts\TbSet as TbSetContract;
+use tiFy\Support\Proxy\View;
+use WP_Admin_Bar;
 
-final class TbSet extends AppController
+/**
+ * @desc Extension PresstiFy de jeux de fonctionnalités des sites TigreBlanc.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package tiFy\Plugins\TbSet
+ * @version 2.0.35
+ *
+ * USAGE :
+ * Activation
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Dans config/app.php ajouter \tiFy\Plugins\TbSet\TbSetServiceProvider à la liste des fournisseurs de services.
+ * ex.
+ * <?php
+ * ...
+ * use tiFy\Plugins\TbSet\TbSetServiceProvider;
+ * ...
+ *
+ * return [
+ *      ...
+ *      'providers' => [
+ *          ...
+ *          TbSetServiceProvider::class
+ *          ...
+ *      ]
+ * ];
+ *
+ * Configuration
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Dans le dossier de config, créer le fichier tb-set.php
+ * @see /vendor/presstify-plugins/tb-set/Resources/config/tb-set.php
+ */
+class TbSet implements TbSetContract
 {
     /**
-     * CONSTRUCTEUR
-     *
-     * @return void
+     * Instance de l'application
+     * @var Container
      */
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->appAddAction('tify_app_register');
-    }
+    protected $container;
 
     /**
-     * Déclaration d'application connexes
+     * CONSTRUCTEUR.
      *
-     * @param AppsServiceProvider $app Classe de rappel du controleur de fournisseur de services.
+     * @param Container|null $container Instance du conteneur d'injection de dépendances.
      *
      * @return void
      */
-    public function tify_app_register($app)
+    public function __construct(?Container $container)
     {
-        $app->setApp(
-            AdminUi::class,
-            [
-                'admin_bar_menu_logo' => [
-                    [
-                        'id'    => 'tb-logo',
-                        'title' => '<span class="tigreblanc-logo" style="font-size:35px;"></span>',
-                        'href'  => 'http://www.tigreblanc.fr',
-                        'meta'  => [
-                            'title'  => __('A propos de Tigre Blanc', 'tify'),
-                            'target' => '_blank',
-                        ],
-                    ],
-                    [
-                        'id'     => 'tb-logo-site',
-                        'parent' => 'tb-logo',
-                        'title'  => __('Site Officiel de Tigre Blanc', 'tify'),
-                        'href'   => 'http://www.tigreblanc.fr',
-                    ],
-                    [
-                        'id'     => 'tb-logo-external',
-                        'parent' => 'tb-logo',
-                        'group'  => true,
-                        'meta'   => [
-                            'class' => 'ab-sub-secondary',
-                        ],
-                    ],
-                    [
-                        'id'     => 'tb-logo-facebook',
-                        'parent' => 'tb-logo-external',
-                        'title'  => __('Page Facebook', 'tify'),
-                        'href'   => 'http://www.facebook.com/tigreblancdouai',
-                        'meta'   => [
-                            'target' => '_blank',
-                        ],
-                    ],
-                    [
-                        'id'     => 'tb-logo-twitter',
-                        'parent' => 'tb-logo-external',
-                        'title'  => __('Compte Twitter', 'tify'),
-                        'href'   => 'https://twitter.com/TigreBlancDouai',
-                        'meta'   => [
-                            'target' => '_blank',
-                        ],
-                    ],
-                    [
-                        'id'     => 'tb-logo-mailing',
-                        'parent' => 'tb-logo',
-                        'group'  => true,
-                        'meta'   => [
-                            'class' => 'ab-sub-primary',
-                        ],
-                    ],
-                    [
-                        'id'     => 'tb-logo-mailing-contact',
-                        'parent' => 'tb-logo-mailing',
-                        'title'  => __('Contact l\'agence', 'tify'),
-                        'href'   => 'mailto:contact@tigreblanc.fr',
-                    ],
-                    [
-                        'id'     => 'tb-logo-mailing-support',
-                        'parent' => 'tb-logo-mailing',
-                        'title'  => __('Support Technique', 'tify'),
-                        'href'   => 'mailto:support@tigreblanc.fr',
+        $this->container = $container;
+
+        // Balises de site.
+        if ($app = $this->getContainer()) {
+            $app->theme([
+                'tag.meta.author'   => 'TigreBlanc Digital',
+                'tag.meta.designer' => 'TigreBlanc'
+            ]);
+        }
+
+        // Personnalisation du logo de la barre d'administration et des sous entrées de menu.
+        add_action('admin_bar_menu', function (WP_Admin_Bar $wp_admin_bar) {
+            $admin_bar_menu_logo = [
+                [
+                    'id'    => 'tb-logo',
+                    'title' => '<span class="tigreblanc-logo" style="font-size:35px;"></span>',
+                    'href'  => 'http://www.tigreblanc.fr',
+                    'meta'  => [
+                        'title'  => __('A propos de Tigre Blanc', 'tify'),
+                        'target' => '_blank',
                     ],
                 ],
-                'admin_footer_text'   => sprintf(
-                    __('Merci de faire de %s le partenaire de votre communication digitale', 'tify'),
-                    "<a class=\"tigreblanc-logo\" href=\"http://www.tigreblanc.fr\" style=\"font-size:40px; vertical-align:middle; display:inline-block;\" target=\"_blank\"></a>"
-                )
-            ]
-        );
+                [
+                    'id'     => 'tb-logo-site',
+                    'parent' => 'tb-logo',
+                    'title'  => __('Site Officiel de Tigre Blanc', 'tify'),
+                    'href'   => 'http://www.tigreblanc.fr',
+                ],
+                [
+                    'id'     => 'tb-logo-external',
+                    'parent' => 'tb-logo',
+                    'group'  => true,
+                    'meta'   => [
+                        'class' => 'ab-sub-secondary',
+                    ],
+                ],
+                [
+                    'id'     => 'tb-logo-facebook',
+                    'parent' => 'tb-logo-external',
+                    'title'  => __('Page Facebook', 'tify'),
+                    'href'   => 'http://www.facebook.com/tigreblancdouai',
+                    'meta'   => [
+                        'target' => '_blank',
+                    ],
+                ],
+                [
+                    'id'     => 'tb-logo-twitter',
+                    'parent' => 'tb-logo-external',
+                    'title'  => __('Compte Twitter', 'tify'),
+                    'href'   => 'https://twitter.com/TigreBlancDouai',
+                    'meta'   => [
+                        'target' => '_blank',
+                    ],
+                ],
+                [
+                    'id'     => 'tb-logo-mailing',
+                    'parent' => 'tb-logo',
+                    'group'  => true,
+                    'meta'   => [
+                        'class' => 'ab-sub-primary',
+                    ],
+                ],
+                [
+                    'id'     => 'tb-logo-mailing-contact',
+                    'parent' => 'tb-logo-mailing',
+                    'title'  => __('Contact l\'agence', 'tify'),
+                    'href'   => 'mailto:contact@tigreblanc.fr',
+                ],
+                [
+                    'id'     => 'tb-logo-mailing-support',
+                    'parent' => 'tb-logo-mailing',
+                    'title'  => __('Support Technique', 'tify'),
+                    'href'   => 'mailto:support@tigreblanc.fr',
+                ],
+            ];
+
+            $wp_admin_bar->remove_menu('wp-logo');
+
+            foreach ($admin_bar_menu_logo as $node) {
+                if (!empty($node['group'])) {
+                    $wp_admin_bar->add_group($node);
+                } else {
+                    $wp_admin_bar->add_menu($node);
+                }
+            }
+        }, 11);
+
+        // Personnalisation du pied de page de l'interface d'administration.
+        add_filter('admin_footer_text', function () {
+            return sprintf(
+                __('Merci de faire de %s le partenaire de votre communication digitale', 'tify'),
+                "<a class=\"tigreblanc-logo\" 
+                    href=\"http://www.tigreblanc.fr\" 
+                    title=\"Tigre Blanc | Agence de communication, agence web à Douai\"
+                    style=\"font-size:40px; 
+                    vertical-align:middle; 
+                    display:inline-block;\" 
+                    target=\"_blank\"
+                    >
+                </a>"
+            );
+        }, 999999);
+
+        // Url du logo de l'interface de connection de Wordpress.
+        add_filter('login_headerurl', function () {
+            return home_url('/');
+        });
+
+        // Intitlulé du lien de l'interface de connection de Wordpress.
+        add_filter('login_headertext', function () {
+            return get_bloginfo('name') . ' | ' . get_bloginfo('description');
+        });
+
+        //Ajout du code google analytics dans le wp_head
+        add_action('wp_head', function () {
+            if ($ua = config('tb-set.ua-code')) {
+                echo View::getPlatesEngine()->setDirectory(__DIR__ . '/Resources/views/ua-code')
+                    ->render('index', compact('ua'));
+            }
+        }, 999999);
     }
 
     /**
-     * Initialisation du controleur.
+     * Récupération du conteneur d'injection de dépendances.
      *
-     * @return void
+     * @return Container|null
      */
-    public function appBoot()
+    public function getContainer(): ?Container
     {
-        $this->appAddAction('init');
-        $this->appAddAction('theme_before_enqueue_scripts');
-        $this->appAddAction('admin_enqueue_scripts');
+        return $this->container;
     }
 
     /**
-     * Initialisation globale de Wordpress.
+     * Définition du conteneur d'injection de dépendances.
      *
-     * @return void
+     * @param Container $container
+     *
+     * @return static
      */
-    public function init()
+    public function setContainer(Container $container): TbSetContract
     {
-        \wp_register_style(
-            'tiFyPluginTbSet',
-            $this->appUrl() . '/assets/fonts/tb/styles.css',
-            current_time('timestamp')
-        );
-    }
+        $this->container = $container;
 
-    /**
-     * Mise en file des scripts dans l'interface utilisateur.
-     *
-     * @return void
-     */
-    public function theme_before_enqueue_scripts()
-    {
-        if ($this->appConfig('wp_enqueue', true)) :
-            \wp_enqueue_style('tiFyPluginTbSet');
-        endif;
-    }
-
-    /**
-     * Mise en file des scripts dans l'interface administrateur.
-     *
-     * @return void
-     */
-    public function admin_enqueue_scripts()
-    {
-        \wp_enqueue_style('tiFyPluginTbSet');
+        return $this;
     }
 }
